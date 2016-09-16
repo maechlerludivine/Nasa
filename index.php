aa511dd42aae55ff65981d53bda20fc1d8ec0479
ECVD-PA-Q084-IM020 <ECVD-IM020@ECVD-PA-Q084-IM020.local>
2016-09-16 10:47:14 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<title>Nasa</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/main.css">
+	</head>
+	<body>
+		<h1>Test</h1>
+		<script src="js/main.js"></script>
+	</body>
+</html>
